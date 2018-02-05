b4699e2e34dbc0c14517977c8526c5855a966262
ChloeLiang <nliangxin@gmail.com>
2018-02-05 20:37:07 +0800

    Detect pins have settled 2

--- Assets/Scripts/PinSetter.cs
+++ Assets/Scripts/PinSetter.cs
@@ -4,18 +4,47 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class PinSetter : MonoBehaviour {
+	public int lastStandingCount = -1;
 	public Text standingDisplay;
 
+	private Ball ball;
 	private bool ballEnteredBox = false;
+	private float lastChangeTime;
 
 	// Use this for initialization
 	void Start () {
-		
+		ball = GameObject.FindObjectOfType<Ball>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		standingDisplay.text = CountStanding().ToString();
+
+		if (ballEnteredBox) {
+			CheckStanding();
+		}
+	}
+
+	// Update the lastStandingCount
+	// Call PinsHaveSettled() when they have
+	void CheckStanding() {
+		int currentStanding = CountStanding();
+		if (currentStanding != lastStandingCount) {
+			lastChangeTime = Time.time;
+			lastStandingCount = currentStanding;
+			return;
+		}
+		float settleTime = 3f;	// How long to wait to consider pins settled
+		if ((Time.time - lastChangeTime) > settleTime) {
+			PinsHaveSettled();
+		}
+	}
+
+	void PinsHaveSettled() {
+		ball.Reset();
+		lastStandingCount = -1;	// Indicates pins have settled, and ball not back in box
+		ballEnteredBox = false;
+		standingDisplay.color = Color.green;
 	}
 
 	int CountStanding() {

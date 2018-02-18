5110bcb9abf9450f732e706cd2597edf1c518337
ChloeLiang <nliangxin@gmail.com>
2018-02-18 16:31:19 +0800

    Wire up ActionMaster

--- Assets/Scripts/PinSetter.cs
+++ Assets/Scripts/PinSetter.cs
@@ -12,16 +12,20 @@ public class PinSetter : MonoBehaviour {
 	private Ball ball;
 	private bool ballEnteredBox = false;
 	private float lastChangeTime;
+	private int lastSettledCount = 10;
+	private ActionMaster actionMaster = new ActionMaster();
+	private Animator animator;
 
 	// Use this for initialization
 	void Start () {
 		ball = GameObject.FindObjectOfType<Ball>();
+		animator = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		standingDisplay.text = CountStanding().ToString();
-
+		//standingDisplay.text = CountStanding().ToString();
 		if (ballEnteredBox) {
 			UpdateStandingCountAndSettle();
 		}
@@ -41,6 +45,9 @@ public class PinSetter : MonoBehaviour {
 
 	public void RenewPins() {
 		Instantiate(pinSet, new Vector3(0, distanceToRaise, 1829), Quaternion.identity);
+		foreach (Pin pin in GameObject.FindObjectsOfType<Pin>()) {
+			pin.GetComponent<Rigidbody>().useGravity = false;
+		}
 	}
 
 	// Update the lastStandingCount
@@ -59,6 +66,23 @@ public class PinSetter : MonoBehaviour {
 	}
 
 	void PinsHaveSettled() {
+		int standing = CountStanding();
+		int pinFall = lastSettledCount - standing;
+		lastSettledCount = standing;
+
+		ActionMaster.Action action = actionMaster.Bowl(pinFall);
+		if (action == ActionMaster.Action.Tidy) {
+			animator.SetTrigger("tidyTrigger");
+		} else if (action == ActionMaster.Action.EndTurn) {
+			animator.SetTrigger("resetTrigger");
+			lastSettledCount = 10;
+		} else if (action == ActionMaster.Action.Reset) {
+			animator.SetTrigger("resetTrigger");
+			lastSettledCount = 10;
+		} else if (action == ActionMaster.Action.EndGame) {
+			throw new UnityException("Don't know how to handle end game yet");
+		}
+
 		ball.Reset();
 		lastStandingCount = -1;	// Indicates pins have settled, and ball not back in box
 		ballEnteredBox = false;
